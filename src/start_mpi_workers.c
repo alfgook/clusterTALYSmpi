6e03ce3d17cb760546f3341940eae642dee23ded
alfgook <alg.gook@physics.uu.se>
2022-05-02 11:10:04 +0200

    fix of binpath

--- src/start_mpi_workers.c
+++ src/start_mpi_workers.c
@@ -62,9 +62,9 @@ int start_mpi_workers(const char **worker_program ,
    }
 
    nbr_of_workers = MIN(nbr_of_workers,universe_size-1);
-   printf("nbr_of_workers = %d\n",nbr_of_workers);
-   printf("nbr_of_jobs = %d\n",nbr_of_jobs);
-   printf("universe_size = %d\n",universe_size);
+   //printf("nbr_of_workers = %d\n",nbr_of_workers);
+   //printf("nbr_of_jobs = %d\n",nbr_of_jobs);
+   //printf("universe_size = %d\n",universe_size);
    //printf("job_list[0] = %s\n",job_list[0]);
 
    // copy the list of jobs from the calling R-script and 
